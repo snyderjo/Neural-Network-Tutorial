965b95cc0400d909c857bd277db5592376e2f8fa
John Snyder <snyderjo@gmail.com>
2018-11-01 19:43:54 -0600

    Added a hyperparameterized hidden layer, allowing for regularization, dropout, gradient clipping, and gradient...reduction(?).  Needs to be tested.  Need to update neuralNet to pass and/or update hyperparameters.

--- nnfiles/hiddenLayer.py
+++ nnfiles/hiddenLayer.py
@@ -5,11 +5,10 @@ from activation_functions import *
 
 class baseHiddenLayer:
 
-	def __init__(self, n_self, n_prev, name, act_func = relu, alpha = .05):
+	def __init__(self, n_self, n_prev, name, act_func = relu,**kwargs):
 		self.name = name
 		self.n_nodes = n_self
 		self.activation_func = act_func
-		self.alpha = alpha
 
 		self.W = np.divide(np.random.randn(n_self,n_prev).astype(np.float128) , np.sqrt(n_prev))
 		self.b = np.zeros((n_self,1),dtype = np.float128)
@@ -19,6 +18,8 @@ class baseHiddenLayer:
 
 		self.A_prev = np.zeros([], dtype = np.float128)
 		self.Z = np.zeros([], dtype = np.float128)
+
+		self.__dict__.update(kwargs)
 
 	def forward(self,A_prev):
 		self.A_prev = A_prev
@@ -48,7 +49,67 @@ class baseHiddenLayer:
 		self.alpha = newAlpha
 
 
-class hiddenLayerDropOut(baseHiddenLayer):
-	def __init__(self,p_drop = .5):
-		self.p_drop = p_drop
+class hiddenLayerWHyperparameters(baseHiddenLayer):
+	def __init__(self,hyperparams = {"alpha":.05, "p_drop":0.0, "lambd":0, "regN":2, "gradThresh" = np.inf, "gradClip": = np.inf}):
+		self.alpha = hyperparams["alpha"]
+		self.lambd = hyperparams["lambd"]
+		self.regN = hyperparams["regN"]
+		self.p_drop = hyperparams["p_drop"]
+		self.gradThresh = hyperparams["gradThresh"]
+
+	def reglatrization_summand(self):
+		regLossSummand = np.zeros(*self.W.shape, dtype = np.float128)
+		if self.regN == 1:
+			regLossSummand = np.multiply(self.lambd,np.abs(self.W))
+
+		elif self.regN == 2:
+			regLossSummand = np.multiply(self.lambd,np.square(self.W))
+
+		else:
+			raise ValueError
+
+		return regLossSummand
+
+	def regulatrization_summand_delta(self):
+		dRegLossSummand = np.zeros(*self.W.shape, dtype = np.float128)
+
+		if self.regN == 1:
+			cond = self.W > 0
+			dRegLossSummand = np.sum(1*cond, -1*(np.logical_not(cond)))
+
+		elif self.regN == 2:
+			dRegLossSummand = np.multiply(self.lambd,self.W)
+
+		else:
+			raise ValueError
+
+		return dRegLossSummand
+
+	def update(self):
+		self.clipGrads()
+
+		self.W = np.subtract(self.W , np.multiply(self.alpha , np.sum(self.dW, self.regulatrization_summand_delta())))
+		self.b = np.subtract(self.b , np.multiply(self.alpha , self.db))
+
+
+	def clipGrads(self):
+		l2_norm = np.sum(np.square(self.dW))
+		if l2_norm > self.gradThresh:
+			self.dW *= np.divide(self.gradThresh,l2_norm)
+
+	def forward(self,A_prev):
+		self.A_prev = A_prev
+		self.Z = np.add(np.matmul(self.W,A_prev) , self.b)
+
+		ActMat = self.activation_func.f(self.Z)
+
+		U = np.divide(np.random.rand(*ActMat.shape) > self.p_drop),self.p_drop)
+
+		ActMat *= U
+
+		return ActMat
+
+
+
+
 
